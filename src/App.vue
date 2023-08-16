545c83933553e9dfb1c72fee566184b16226b898
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-08-16 10:56:46 +0200

    initial commit for pokedex with vue js

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header title="Pokedex with Vue JS" />
+</template>
+
+<style scoped lang="scss">
+$color : #368ff4;
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ title: string }>()
+
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+
+  </header>
+
+
+</template>
+
+<style scoped lang="scss">
+
+
+
+header {
+  background-color: #f44336;
+  padding: 10px;
+  text-align: center;
+  
+  h1 {
+    font-size: 2rem;
+    margin: 0;
+    color: white;
+    font-weight: 700;
+  }
+}
+
+</style>
