cce3894a207aa1f9183bef469f9c0d8e60d9d827
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-08-17 19:19:25 +0200

    work on header style + responsive.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,10 @@ defineProps<{ title: string }>()
 
 <template>
   <header>
-    <h1>{{ title }}</h1>
+    <div class="header_bg">
+      <h1>{{ title }}</h1>
+
+    </div>
 
   </header>
 
@@ -15,18 +18,43 @@ defineProps<{ title: string }>()
 
 <style scoped lang="scss">
 
-
-
 header {
-  background-color: #f44336;
-  padding: 10px;
-  text-align: center;
+  // padding: 10px;
   
   h1 {
-    font-size: 2rem;
+    font-size: 1rem;
     margin: 0;
     color: white;
     font-weight: 700;
+    display: inline;
+    position: absolute;
+    top: 0.1rem;
+    right: 1rem;
+    @media screen and (min-width: 500px) {
+      font-size: 1.5rem;
+    }
+    @media screen and (min-width: 700px) {
+      font-size: 2rem;
+    }
+    @media screen and (min-width: 1000px) {
+      font-size: 2.5rem;
+      right: 3rem;
+      top: 0.5rem;
+    }
+  }
+}
+
+.header_bg {
+  background: no-repeat url(public/img/header.png) center top / contain;
+  width: 100%;
+  margin: 0 auto;
+  height: 30vh;
+  position: relative;
+  @media screen and (min-width: 768px) {
+    height: 23vh;
+  }
+  @media screen and (min-width: 1180px) {
+    width: 1180px;
   }
 }
 
