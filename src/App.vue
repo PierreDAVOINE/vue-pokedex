795fd0384c1a421ef7c8612554f30ce828ffca89
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-08-21 19:29:43 +0200

    some try to connect inputSearch state  to the input formulaire

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import { Pokemon } from './@types/Pokemons';
 
 const pokemons = ref([] as Pokemon[]);
+const inputSearch = ref('Pokem' as string);
 
 const getPokemons = async () => {
   try {
     const response = await fetch('https://pokebuildapi.fr/api/v1/pokemon/limit/100');
     const data = await response.json();
-    console.log("pokemons", data);
     pokemons.value = data;
   } catch (error) {
     console.log(error);
@@ -21,10 +21,15 @@ onMounted(() => {
   getPokemons();
 })
 
+//  A chaque mise à jour de inputSearch on le console.log
+watch(inputSearch, (newValue: string) => {
+  console.log("Nouvelle valeurde l'input : ", newValue);
+})
+
 </script>
 
 <template>
-  <Header />
+  <Header :inputSearch="inputSearch" />
   <Main :pokemons="pokemons" />
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import { ref, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  inputSearch: {
+    type: String,
+    required: true,
+  },
+});
+// TODO: Relier la valeur de l'input à la valeur de inputSearch
+// v-model ne peut pas être utilisé sur une props
+const emit = defineEmits(['update:inputSearch']);
+const inputValue = ref(props.inputSearch);
+
+const updateInputSearch = (event: Event) => {
+  const newValue = (event.target as HTMLInputElement).value;
+  inputValue.value = newValue;
+  emit('update:inputSearch', newValue);
+};
+
 </script>
 
 <template>
   <header>
     <div class="header_bg">
-      <h1>Pokedex with Vue JS</h1>
-
+      <h1>Pokedex with Vue JS = {{ props.inputSearch }}</h1>
+      <form>
+        <input type="text" :value="props.inputSearch" @input="(e) => updateInputSearch(e)"
+          placeholder="Search a pokemon" />
+      </form>
     </div>
 
   </header>
