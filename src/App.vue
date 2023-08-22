ab9fc04021545ad89a0a26b708c76f3151277cac
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-08-22 19:34:48 +0200

    update of search form. the input search is working great with a watch funtion + filter() with includes().

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,16 @@ import Main from './components/Main.vue'
 import { Pokemon } from './@types/Pokemons';
 
 const pokemons = ref([] as Pokemon[]);
-const inputSearch = ref('Pokem' as string);
+const inputSearch = ref('' as string);
+const pokemonFiltered = ref([] as Pokemon[]);
 
 const getPokemons = async () => {
   try {
-    const response = await fetch('https://pokebuildapi.fr/api/v1/pokemon/limit/100');
+    const response = await fetch('https://pokebuildapi.fr/api/v1/pokemon/limit/152');
+    // const response = await fetch('https://pokebuildapi.fr/api/v1/pokemon');
     const data = await response.json();
     pokemons.value = data;
+    pokemonFiltered.value = data;
   } catch (error) {
     console.log(error);
   }
@@ -24,13 +27,24 @@ onMounted(() => {
 //  A chaque mise à jour de inputSearch on le console.log
 watch(inputSearch, (newValue: string) => {
   console.log("Nouvelle valeurde l'input : ", newValue);
+  if (newValue.length > 0) {
+    pokemonFiltered.value = pokemons.value.filter((pokemon: Pokemon) => {
+      return pokemon.name.toLowerCase().includes(newValue.toLowerCase());
+    })
+  } else {
+    pokemonFiltered.value = pokemons.value;
+  }
 })
+
+const updateInputSearch = (newValue: string) => {
+  inputSearch.value = newValue;
+}
 
 </script>
 
 <template>
-  <Header :inputSearch="inputSearch" />
-  <Main :pokemons="pokemons" />
+  <Header :inputSearch="inputSearch" @update:inputSearch="updateInputSearch" />
+  <Main :pokemons="pokemonFiltered" />
 </template>
 
 <style scoped lang="scss"></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,60 @@
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref } from 'vue';
 
+// On récupére la props inputSearch
 const props = defineProps({
   inputSearch: {
     type: String,
     required: true,
   },
 });
-// TODO: Relier la valeur de l'input à la valeur de inputSearch
-// v-model ne peut pas être utilisé sur une props
+
+// On déclare la fonction update:inputSearch
+// emit permet d'envoyer un événement au parent
 const emit = defineEmits(['update:inputSearch']);
+
+// On déclare une variable inputValue qui est une référence à la props inputSearch
 const inputValue = ref(props.inputSearch);
 
+// updateInputSearch permet de mettre à jour la props inputSearch
 const updateInputSearch = (event: Event) => {
   const newValue = (event.target as HTMLInputElement).value;
   inputValue.value = newValue;
+  // ici on emet l'evenement update:inputSearch avec la nouvelle valeur
   emit('update:inputSearch', newValue);
 };
 
+// handleReset permet de remettre à zéro la props inputSearch et donc de reset la recherche
+const handleReset = () => {
+  inputValue.value = '';
+  emit('update:inputSearch', '');
+};
+
+// handleSubmit permet d'éviter le rechargement de la page lors de la soumission du formulaire
+const handleSubmit = (event: Event) => {
+  event.preventDefault();
+};
 </script>
 
 <template>
   <header>
     <div class="header_bg">
-      <h1>Pokedex with Vue JS = {{ props.inputSearch }}</h1>
-      <form>
-        <input type="text" :value="props.inputSearch" @input="(e) => updateInputSearch(e)"
-          placeholder="Search a pokemon" />
-      </form>
+      <h1>Pokedex with Vue JS</h1>
     </div>
 
+    <form v-on:submit="handleSubmit">
+      <input type="text" :value="props.inputSearch" @input="(e) => updateInputSearch(e)" placeholder="Search a pokemon" />
+      <button v-if="props.inputSearch.length > 0" v-on:click="handleReset">
+        <img src="../assets/icons8-annuler.svg" alt="annuler">
+      </button>
+    </form>
   </header>
 </template>
 
 <style scoped lang="scss">
 header {
-  // padding: 10px;
+  max-width: 850px;
+  margin: 0 auto;
 
   h1 {
     font-size: 1rem;
@@ -55,17 +74,55 @@ header {
       font-size: 2rem;
     }
   }
+
+  form {
+    position: relative;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 850px;
+    padding: 1rem;
+    background-color: #ec1d23;
+
+    input {
+      width: 100%;
+      padding: 0.5rem;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      font-size: 1rem;
+      font-weight: 400;
+      color: #333;
+      box-shadow: 0 3px 15px rgba(100, 100, 100, 0.2);
+
+      @media screen and (min-width: 500px) {
+        font-size: 1.5rem;
+      }
+
+      @media screen and (min-width: 700px) {
+        font-size: 2rem;
+      }
+    }
+
+    button {
+      position: absolute;
+      top: 1rem;
+      right: 1rem;
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+  }
 }
 
 .header_bg {
   background: no-repeat url(/img/header.png) center top / contain, #ec1d23;
   width: 100%;
   margin: 0 auto;
-  height: 30vh;
+  height: 6vh;
   position: relative;
 
-  @media screen and (min-width: 768px) {
-    height: 23vh;
+  @media screen and (min-width: 720px) {
+    height: 16vh;
   }
 
   @media screen and (min-width: 850px) {
