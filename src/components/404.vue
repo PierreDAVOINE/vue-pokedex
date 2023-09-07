f008aa990c911838457edbf7c2b052a8a07e2b58
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-09-07 17:16:46 +0200

    Update of 404 page add a picture and update style

--- src/components/404.vue
+++ src/components/404.vue
@@ -4,22 +4,34 @@
 
 <template>
     <div class="error404">
-        <h2>Pokemon non trouvé</h2>
-
+        <h2>Aucun pokemon trouvé avec ce nom</h2>
+        <img src="../assets/404.png" alt="404 not found">
     </div>
 </template>
 
 <style scoped lang="scss">
 .error404 {
-    max-width: 850px;
+    width: 100vw;
+    max-width: 700px;
     margin: 0 auto;
-    padding: 1rem;
     text-align: center;
+    overflow-y: scroll;
+    height: 75vh;
+    border-right: 7px solid black;
+    border-left: 7px solid black;
+    overscroll-behavior-y: none;
+    background-color: #ec1d23;
+    position: relative;
 
     h2 {
-        color: rgb(189, 0, 222);
+        color: white;
         font-size: 3rem;
         font-weight: 700;
+    }
+
+    img {
+        width: 55%;
+        max-width: 500px;
     }
 }
 </style>
